02f44abaf5d690639f9ac7fe0df729c227c6142d
yorelmoore-ctrl <yorelmoore@gmail.com>
2026-07-26 21:08:30 -0400

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,20 @@
+const affirmations = [
 
+"You are stronger than you know.",
+
+"Your worth isn't measured by productivity.",
+
+"Rest is productive.",
+
+"You deserve kindness today.",
+
+"One step at a time.",
+
+"Small victories still matter."
+
+];
+
+const random = Math.floor(Math.random() * affirmations.length);
+
+document.getElementById("affirmation").textContent =
+affirmations[random];
