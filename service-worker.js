99a2a4e8ec675d5b228ff9b5d177b4f9e18c646f
yorelmoore-ctrl <yorelmoore@gmail.com>
2026-07-28 17:26:51 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,131 @@
+// Yorel Versus MS Service Worker
 
+const CACHE_NAME = "yorel-versus-ms-v1";
+
+
+const FILES_TO_CACHE = [
+
+    "./",
+
+    "./index.html",
+
+    "./style.css",
+
+    "./script.js",
+
+    "./manifest.json",
+
+    "./assets/icons/icon-192.png",
+
+    "./assets/icons/icon-512.png"
+
+];
+
+
+
+
+// Install App Files
+
+self.addEventListener(
+"install",
+event => {
+
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+        .then(cache => {
+
+            return cache.addAll(
+                FILES_TO_CACHE
+            );
+
+        })
+
+    );
+
+
+});
+
+
+
+
+
+
+// Load Cached Files
+
+self.addEventListener(
+"fetch",
+event => {
+
+
+    event.respondWith(
+
+        caches.match(
+            event.request
+        )
+
+        .then(response => {
+
+
+            return response ||
+
+            fetch(
+                event.request
+            );
+
+
+        })
+
+    );
+
+
+});
+
+
+
+
+
+
+// Update Cache
+
+self.addEventListener(
+"activate",
+event => {
+
+
+    event.waitUntil(
+
+        caches.keys()
+
+        .then(keys => {
+
+
+            return Promise.all(
+
+                keys.map(key => {
+
+
+                    if(
+                        key !== CACHE_NAME
+                    ){
+
+                        return caches.delete(
+                            key
+                        );
+
+                    }
+
+
+                })
+
+            );
+
+
+        })
+
+    );
+
+
+});
